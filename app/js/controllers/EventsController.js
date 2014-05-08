9166cee48afa93a086ad55d7c67127ee4d07f049
Ben Diamant <bend@checkpoint.com>
2014-05-08 13:03:17 -0400

    Initial project for statistic task.

--- app/js/controllers/EventsController.js
+++ app/js/controllers/EventsController.js
@@ -8,8 +8,37 @@ eventsApp.controller('EventController',
             name: 'Angular Boot Camp',
             date: '1/1/2013',
             time: '10:30 am',
-            imageUrl: '/img/AngularJS-large.png'
+            location: {
+                address: 'Bend Appartment',
+                city: 'Mouuntain View',
+                province: 'CA'
+            },
+            imageUrl: '/img/AngularJS-large.png',
+            sessions: [
+                {
+                    name: 'Session number 2',
+                    creationName: 'Bend',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Session number 1',
+                    creationName: 'Moshe',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Session number 3',
+                    creationName: 'David',
+                    upVoteCount: 0
+                }
+            ]
         }
+        $scope.upVoteSession = function(session) {
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function(session) {
+            session.upVoteCount--;
+        };
 
 
 });
